6a9637c002015a3099db1e1a0dec6ee324515600
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-23 23:17:41 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -53,7 +53,7 @@ if user_answer === 'help'
 elsif user_answer === 'list'
   list(songs)
 elsif user_answer === 'play'
-  play(list(songs))
+  play(songs[user_answer.to_i])
 elsif user_answer === 'exit'
   exit_jukebox
   #binding.pry
